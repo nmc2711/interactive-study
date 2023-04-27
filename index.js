0baf22dca6d57c567fb936d4abc5772c9c733149
황상한 <55230278+nmc2711@users.noreply.github.com>
2023-04-27 16:26:26 +0900

    캔버스 비내리는 효과까지 구현

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const dpr = window.devicePixelRatio;
 
-const canvasWidth = 300;
-const canvasHeigth = 300;
+const canvasWidth = innerWidth;
+const canvasHeigth = innerHeight;
 
 canvas.style.width = canvasWidth + 'px';
 canvas.style.height = canvasHeigth + 'px';
@@ -13,4 +13,72 @@ canvas.width = canvasWidth * dpr;
 canvas.height = canvasHeigth * dpr;
 ctx.scale(dpr, dpr);
 
-ctx.fillRect(10, 10, 50, 50);
+class Particle {
+  constructor(x, y, radius, vy) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.vy = vy;
+    this.acc = 1.01;
+  }
+
+  update() {
+    this.vy *= this.acc;
+    this.y += this.vy;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, (Math.PI / 180) * 360);
+    ctx.fillStyle = 'orange';
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+const TOTAL = 20;
+
+const randomNumBetween = (min, max) => {
+  return Math.random() * (max - min + 1) + min;
+};
+
+let particles = [];
+
+for (let i = 0; i < TOTAL; i++) {
+  const x = randomNumBetween(0, canvasWidth);
+  const y = randomNumBetween(0, canvasHeigth);
+  const vy = randomNumBetween(1, 5);
+  const radius = randomNumBetween(50, 100);
+  const particle = new Particle(x, y, radius, vy);
+
+  particles.push(particle);
+}
+
+const interval = 1000 / 60;
+let now, delta;
+let then = Date.now();
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  now = Date.now();
+  delta = now - then;
+
+  if (delta < interval) return;
+  ctx.clearRect(0, 0, canvasWidth, canvasHeigth);
+
+  particles.forEach((particle) => {
+    particle.update();
+    particle.draw();
+
+    if (particle.y - particle.radius > canvasHeigth) {
+      particle.y = -particle.radius;
+      particle.x = randomNumBetween(0, canvasWidth);
+      particle.vy = randomNumBetween(1, 5);
+      particle.radius = randomNumBetween(50, 100);
+    }
+  });
+
+  then = now - (delta % interval);
+}
+
+animate();
